b8d8df0638adc697c09294fec4c6fe62374ab1f2
kevrock <kevinroque.dev@gmail.com>
2025-07-10 22:40:22 +0800

    Updates

---
+++ assets/AboutView-Dm_HSjLI.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,a as s,o as t}from"./index-SoV5xeLG.js";const c={},a={class:"about"},n=s("h1",null,"This is an about page",-1),_=[n];function r(i,d){return t(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
